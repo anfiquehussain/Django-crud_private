029a5f608deab830fb1242ecf34bd05c2ac369ef
anfique hussain v <anfiquehussain1@gmail.com>
2024-05-22 14:03:04 +0530

    updated

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@2=rthudrvs^@p+l_fd2yay-ond2+*5w!u3w8(9azwsywc)w!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'True'
 
-ALLOWED_HOSTS = [.vercel.app]
+ALLOWED_HOSTS = []
 
 
 # Application definition
