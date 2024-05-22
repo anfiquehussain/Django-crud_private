29e07dafcf4e033510c229d7588786537b907777
anfique hussain v <anfiquehussain1@gmail.com>
2024-05-22 14:50:41 +0530

    updated code render

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@2=rthudrvs^@p+l_fd2yay-ond2+*5w!u3w8(9azwsywc)w!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -76,14 +79,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'crud_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse("postgres://jobnest_user:NyOyH12hERfnn91mcN29DHAzfW9mOKz7@dpg-cp6rdra0si5c73aj5170-a.oregon-postgres.render.com/jobnest")
 }
 
 
@@ -122,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
