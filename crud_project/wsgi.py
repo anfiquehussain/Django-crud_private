094617f259562685645fdf1be258fa744dd5855a
Anfique Hussain V <90567860+anfiquehussain@users.noreply.github.com>
2024-05-22 14:59:02 +0530

    Update wsgi.py

--- crud_project/wsgi.py
+++ crud_project/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud_project.settings')
 
-application = get_wsgi_application()
-
-app = application
+application = get_wsgi_application()n
