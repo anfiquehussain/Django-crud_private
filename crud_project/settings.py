c942549b8ddb8ffd78b823d319450eaa4dff30a1
anfique hussain v <anfiquehussain1@gmail.com>
2024-05-22 11:10:32 +0530

    vercel added

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@2=rthudrvs^@p+l_fd2yay-ond2+*5w!u3w8(9azwsywc)w!y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [.vercel.app]
 
 
 # Application definition
